02a4248a4fada20c468e1cb84f27d599432e3ae4
LiamWatson-Clouza <153590157+LiamWatson-Clouza@users.noreply.github.com>
2024-02-15 16:36:31 +0200

    added config for dynamodb

--- team-table-tennis/src/App.js
+++ team-table-tennis/src/App.js
@@ -1,6 +1,35 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
 import "./App.css";
+import * as AWS from "aws-sdk";
+
+AWS.config.update({
+  endpoint: "http://localhost:8000",
+});
+
+this.dynamodb = new AWS.DynamoDB();
+this.docClient = new AWS.DynamoDB.DocumentClient();
+
+onRead = () => {
+  let that = this;
+  let params = {
+    TableName: "leaderboard-data",
+  };
+
+  this.docClient.scan(params, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      that.setState({
+        gridData: data,
+      });
+    }
+  });
+};
+<Grid data={this.state.gridData}>
+  <GridColumn title="Name" field="name" />
+  <GridColumn title="Score" field="score" />
+</Grid>;
 
 export default function App() {
   return (
