c1388c6f3b1ced707effdb461ffcf385da5e5abb
LiamWatson-Clouza <153590157+LiamWatson-Clouza@users.noreply.github.com>
2024-02-15 11:25:29 +0200

    changed files

--- team-table-tennis/src/App.js
+++ team-table-tennis/src/App.js
@@ -1,10 +1,38 @@
 import React from "react";
-import "./components/Styles.css";
-function App() {
+import { Table } from "react-bootstrap";
+import "./App.css";
+export default function App() {
   return (
     <div className="App" id="main">
       <h1>Table Tennis Rankings</h1>
+      <div className="board">
+        <div className="leaderboard">
+          <Leaderboard />
+        </div>
+      </div>
     </div>
   );
 }
-export default App;
+
+function Leaderboard() {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Winner Name</th>
+          <th>Score</th>
+          <th>Wins</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Liam</td>
+          <td>11</td>
+          <td>1</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
