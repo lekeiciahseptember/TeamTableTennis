368c11dd53b3538854c7110755fdfd50dd3698e9
LiamWatson-Clouza <153590157+LiamWatson-Clouza@users.noreply.github.com>
2024-02-21 09:33:13 +0200

    Added App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import Leaderboard from "./LeaderBoard";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Leaderboard />
+    </div>
+  );
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Leaderboard from "./LeaderBoard";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Leaderboard />
+    <App />
   </React.StrictMode>
 );
